33e5755bf1bbbcb7241b714e3db8bb45ddeaa8a9
Alessio <alexeimv91@gmail.com>
2023-11-02 15:27:06 -0400

    input de buscar arreglado

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # ...
     path('', views.home, name='home'),
+    path('/search_activities', views.search_activities, name='search_activities'),
     # ...
 ]
--- app/views.py
+++ app/views.py
@@ -1,15 +1,61 @@
 from django.shortcuts import render, redirect
-from .models import Activity
+from .models import Activity, Aspecto
+from django.http import HttpResponse
+from django.db.models import Q
 
 # Create your views here.
 
 def home(request):
+    
     if request.user.is_authenticated:
-        actividades = Activity.objects.all()
+        activities = Activity.objects.all()
         context = {
-            'actividades': actividades
+            'actividades': activities,
+            'flag': True
         }
         
         return render(request, 'actividad.html', context=context)
     else:
         return redirect('login')
+    
+    
+def search_activities(request):
+    search_input = request.GET.get('search_activities', None)
+    # if request.htmx:
+    #     print('funciona')
+    if search_input:
+        
+        
+        
+        patterns =Q(description__icontains=search_input) | Q(aspecto_id__in=Aspecto.objects.filter(name__icontains = search_input).values_list('id', flat=True))
+        activities = Activity.objects.filter(patterns).all()
+        # return render(request, 'actividad.html', {'activities': activities})
+        return HttpResponse(
+                [f"""<tr>
+                                            <td>{activity.description}</td>
+                                            <td>{activity.aspecto}</td>
+                                            <td>{activity.is_open}</td>
+                                            <td class="table-action">
+                                                <a href="javascript: void(0);" class="action-icon text-success"> <i class="mdi mdi-eye"></i></a>
+                                                <a href="javascript: void(0);" class="action-icon text-primary"> <i class="mdi mdi-pencil"></i></a>
+                                                <a href="javascript: void(0);" class="action-icon text-danger"> <i class="mdi mdi-delete"></i></a>
+                                            </td>
+                                        </tr>""" for activity in activities]
+            )
+        # else:
+        #     return HttpResponse(
+        #         '<p class="text-center">Center aligned text on all viewport sizes.</p>'
+        #         )
+    else:
+        activities = Activity.objects.all()
+        return HttpResponse(
+            [f"""<tr>
+                                        <td>{activity.description}</td>
+                                        <td>{activity.aspecto}</td>
+                                        <td>{activity.is_open}</td>
+                                        <td class="table-action">
+                                            <a href="javascript: void(0);" class="action-icon text-success"> <i class="mdi mdi-eye"></i></a>
+                                            <a href="javascript: void(0);" class="action-icon text-primary"> <i class="mdi mdi-pencil"></i></a>
+                                            <a href="javascript: void(0);" class="action-icon text-danger"> <i class="mdi mdi-delete"></i></a>
+                                        </td>
+                                    </tr>""" for activity in activities])
